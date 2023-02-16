eab55c3a496d2ac7eeaa405e8982299168a4d551
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-16 16:23:03 -0700

    test: create health test

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -15,5 +15,13 @@ RSpec.describe Ship do
       expect(cruiser.name).to eq("Cruiser")
       expect(cruiser.length).to eq(3)
     end
+
+    it 'has health' do
+      cruiser = Ship.new("Cruiser", 3)
+
+      expect(cruiser.health).to eq(3)
+    end
+
+
   end
 end
