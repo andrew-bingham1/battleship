cd163a0b65cc310acad8fc4323fda42d3650bd32
Jeff Nelson <jpnelson85@gmail.com>
2023-02-16 17:58:36 -0600

    update place_ship method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,8 +1,14 @@
 class Cell
-  attr_reader :coordinate
+  attr_reader :coordinate,
+              :ship
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
+  end
+
+  def place_ship(ship)
+    @ship = ship
 
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -17,6 +17,6 @@ RSpec.describe Cell do
     cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
-    expect(cell.ship_in_cell).to eq(cruiser)
+    expect(cell.ship).to eq(cruiser)
   end
 end
