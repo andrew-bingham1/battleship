14ee72143af18023cb98cb04b2207403faf580a2
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-16 16:29:30 -0700

    feat: add sunk? method

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,9 +1,14 @@
 class Ship
-attr_reader :name, :length, :health
+attr_reader :name, :length, :health, :sunk
 
   def initialize(name,length)
     @name = name
     @length = length 
     @health = length 
+    @sunk = false 
+  end
+
+  def sunk?
+    @sunk
   end
 end
