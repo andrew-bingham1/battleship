c8719720f26118e1e4c0baee428e924461d7e11d
Jeff Nelson <jpnelson85@gmail.com>
2023-02-17 13:19:45 -0600

    add validating coordinates test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Board do
     board = Board.new
     expect(board.cells).to eq()
   end
+
+  # need additional tests for cells
+  # and need to fix above test
+
+  it 'validating coordinates' do
+    board = Board.new
+    expect(board.valid_coordinate?('A1')).to eq(true)
+    expect(board.valid_coordinate?('D4')).to eq(true)
+    expect(board.valid_coordinate?('A5')).to eq(false)
+    expect(board.valid_coordinate?('E1')).to eq(false)
+    expect(board.valid_coordinate?('A22')).to eq(false)
+  end
 end
