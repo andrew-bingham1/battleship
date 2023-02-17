5606e56fac0f943e603cbb7cc94c017057d505ec
Jeff Nelson <jpnelson85@gmail.com>
2023-02-17 13:37:03 -0600

    acceptable coordinate test

--- lib/board.rb
+++ lib/board.rb
@@ -22,4 +22,6 @@ class Board
      }
   end
 
+  
+
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require './lib/board'
+require './lib/ship'
 
 RSpec.describe Board do
   it 'exists' do
@@ -22,5 +23,21 @@ RSpec.describe Board do
     expect(board.valid_coordinate?('A5')).to eq(false)
     expect(board.valid_coordinate?('E1')).to eq(false)
     expect(board.valid_coordinate?('A22')).to eq(false)
+  end
+
+  it 'number of coordinates equal to ship length, consecutive, and not diagonal' do
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2) 
+    expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A4"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A1", "C1"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A3", "A2", "A1"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq(false)
+    expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to eq(true)
+    expect(board.valid_placement?(submarine, ["B1", "C1"])).to eq(true)
   end
 end
