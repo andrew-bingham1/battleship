a66baf185ba4264de4e4c504e96ac255cc15b546
Jeff Nelson <jpnelson85@gmail.com>
2023-02-19 11:01:57 -0600

    update changes

--- lib/cell.rb
+++ lib/cell.rb
@@ -32,17 +32,32 @@ class Cell
     end
   end
 
+  # def render(show_ship = false)
+  #   if @targeted == false && show_ship == false && self.ship == nil
+  #       return "."
+  #     elsif @targeted == false && show_ship == true && self.ship != nil
+  #       return "S"
+  #     elsif @targeted == true && self.ship == nil
+  #       return "M"
+  #     elsif @targeted == true && self.ship != nil && self.ship.sunk != true
+  #       return "H"
+  #     else @targeted == true && self.ship != nil && self.ship.health == 0
+  #       return "X"
+  #   end
+  # end
+
   def render(show_ship = false)
-    if @targeted == false && show_ship == false
-        return "."
-      elsif @targeted == false && show_ship == true 
-        return "S"
-      elsif @targeted == true && self.ship == nil
-        return "M"
-      elsif @targeted == true && self.ship != nil && self.ship.sunk != true
-        return "H"
-      elsif self.ship.sunk == true 
-        return "X"
+    if self.fired_upon? == false && show_ship == true && self.empty? == false
+      return "S"
+    elsif self.fired_upon? == false && show_ship == false
+      return "."
+    elsif self.fired_upon? == true && self.empty? == true
+      return "M"
+    elsif self.fired_upon? == true && self.empty? == false
+      return "H"
+    elsif self.fired_upon? == true && self.ship.sunk == true
+      return "X"
+
     end
   end
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -87,10 +87,10 @@ RSpec.describe Board do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)  
     board.place(cruiser, ["A1", "A2", "A3"])
-    expect(board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+    expect(board.render).to eq("  1 2 3 4 \nA . . . .\nB . . . .\nC . . . .\nD . . . .")
   end
 
-  xit 'render method with argument true' do
+  it 'render method with argument true' do
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)  
     board.place(cruiser, ["A1", "A2", "A3"])
