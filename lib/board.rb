c44a5cd420ab8e9a9040e391e628b7eb41043e1a
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-20 14:02:31 -0700

    refactor: refactor valid_placement? to better account for overlapping ships

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,7 @@
 class Board
-  attr_reader :cells
+  attr_reader :cells,
+              :valid_cruiser,
+              :valid_sub
 
   def initialize
     @cells = {
@@ -20,6 +22,9 @@ class Board
       "D3" => Cell.new("D3"),
       "D4" => Cell.new("D4")
      }
+     @valid_cruiser = [['A1', 'B1', 'C1'], ['B1', 'C1', 'D1'], ['A2', 'B2', 'C2'], ['B2', 'C2', 'D2'], ['A3', 'B3', 'C3'], ['B3', 'C3', 'D3'], ['A4', 'B4', 'C4'], ['B4', 'C4', 'D4'], ['A1', 'A2', 'A3'], ['B1', 'B2', 'B3'], ['C1', 'C2', 'C3'], ['D1', 'D2', 'D3'], ['A2', 'A3', 'A4'], ['B2', 'B3', 'B4'], ['C2', 'C3', 'C4'], ['D2', 'D3', 'D4']]
+     @valid_sub = [['A1', 'A2'], ['A2', 'A3'], ['A3', 'A4'], ['B1', 'B2'], ['B2', 'B3'], ['B3', 'B4'], ['C1', 'C2'], ['C2', 'C3'], ['C3', 'C4'], ['D1', 'D2'], ['D2', 'D3'], ['D3', 'D4'], ['A1', 'B1'], ['B1', 'C1'], ['C1', 'D1'], ['A2', 'B2'], ['B2', 'C2'], ['C2', 'D2'], ['A3', 'B3'], ['B3', 'C3'], ['C3', 'D3'], ['A4', 'B4'], ['B4', 'C4'], ['C4', 'D4']]
+     
   end
 
   def valid_coordinate?(loc)
@@ -30,38 +35,38 @@ class Board
     end
   end
 
-  def valid_placement?(ship, loc_arr)
-    valid_cruiser = [['A1', 'B1', 'C1'], ['B1', 'C1', 'D1'], ['A2', 'B2', 'C2'], ['B2', 'C2', 'D2'], ['A3', 'B3', 'C3'], ['B3', 'C3', 'D3'], ['A4', 'B4', 'C4'], ['B4', 'C4', 'D4'], ['A1', 'A2', 'A3'], ['B1', 'B2', 'B3'], ['C1', 'C2', 'C3'], ['D1', 'D2', 'D3'], ['A2', 'A3', 'A4'], ['B2', 'B3', 'B4'], ['C2', 'C3', 'C4'], ['D2', 'D3', 'D4']]
-    valid_sub = [['A1', 'A2'], ['A2', 'A3'], ['A3', 'A4'], ['B1', 'B2'], ['B2', 'B3'], ['B3', 'B4'], ['C1', 'C2'], ['C2', 'C3'], ['C3', 'C4'], ['D1', 'D2'], ['D2', 'D3'], ['D3', 'D4'], ['A1', 'B1'], ['B1', 'C1'], ['C1', 'D1'], ['A2', 'B2'], ['B2', 'C2'], ['C2', 'D2'], ['A3', 'B3'], ['B3', 'C3'], ['C3', 'D3'], ['A4', 'B4'], ['B4', 'C4'], ['C4', 'D4']]
-  
-    loc_arr.each do |coor|
-      self.cells.each do |key, value|
-        if coor == key
-          if value.empty? == true
-            if ship.name == "Cruiser"
-              if valid_cruiser.include?(loc_arr) == true && ship.length == loc_arr.length
+ def valid_placement?(ship, coordinates)
+  if ship.length != coordinates.length
+    return false
+  end
+  if ship.name == "Cruiser"
+    if valid_cruiser.include?(coordinates) == false
+      return false 
+    end
+  elsif ship.name == "Submarine"
+    if valid_sub.include?(coordinates) == false 
+      return false
+    end
+  end
+  num_of_trues = 0 
+      coordinates.each do |coor|
+        self.cells.each do |key, value|
+          if coor == key
+            if value.empty? == true 
+               num_of_trues += 1
+              if num_of_trues == ship.length
                 return true
-              else
-                return false
               end
-            elsif ship.name == "Submarine"
-              if valid_sub.include?(loc_arr) == true && ship.length == loc_arr.length
-                return true
-              else
-                return false
-              end
+            elsif value.empty? == false
+              return false
             end
-          elsif value.empty? == false
-            return false
-             
           end
         end
       end
-    end
-  end
+ end
+  
 
   def place(ship_name, coordinates) 
-    # if self.valid_placement?((ship_name), (coordinates)) == true 
       coordinates.each do |coor|
         self.cells.each do |key, value|
           if coor == key
@@ -70,7 +75,6 @@ class Board
           end
         end
       end 
-    # end
   end
 
   def render(board_show_ship = false)
@@ -92,17 +96,3 @@ class Board
 end
 
 
- # def valid_placement?(ship, loc_arr)
-  #   range1 = 'A1'..'A4'
-  #   range2 = 'B1'..'B4' 
-  #   range3 = 'C1'..'C4'
-  #   range4 = 'D1'..'D4'
-  #   superrange = [range1, range2, range3, range4] 
-  #   superrange.each do |range|
-  #     if range.each_cons(ship.length).to_a.include?(loc_arr) == true && ship.length == loc_arr.length
-  #       return true
-  #     else
-  #       return false 
-  #     end
-  #   end
-  # end
--- lib/game.rb
+++ lib/game.rb
@@ -6,18 +6,39 @@ class Game
 
     case input
       when "p"
+        print "Let's begin!"
         computer = Board.new 
         player = Board.new 
-        print "These are the game boards.\nThe top one is mine \nand the bottom is yours.\n" + computer.render + "\n" +  "===========\n" + player.render(true) 
+        cruiser_p = Ship.new("Cruiser", 3)
+        submarine_p = Ship.new("Submarine", 2)
+        cruiser_c = Ship.new("Cruiser", 3)
+        submarine_c = Ship.new("Submarine", 2) 
 
-        ### computer then takes a random sampling for 
-        ### crusier and submarine based off hardcoded 
-        ### valid placements and calls on the #place method 
+        computer_sub_loc = computer.valid_sub.sample 
+        computer.place(submarine_c , computer_sub_loc)
+      
+        computer_cru_loc = computer.valid_cruiser.sample
+        loop do 
+          if computer.valid_placement?(cruiser_c, computer_cru_loc) == true
+            computer.place(cruiser_c, computer_cru_loc)
+            break
+          else
+            computer_cru_loc = computer.valid_cruiser.sample
+          end
+        end
 
         print "I have laid out my ships on the grid.
         You now need to lay out your two ships.
         The Cruiser is three units long and the Submarine is two units long.
         Enter the squares for the Cruiser (3 spaces)"
+
+
+        require 'pry'; binding.pry
+         
+
+      
+
+        
 
         ### program now takes user input and renders it into 
         ### useable info for the place method
@@ -28,14 +49,16 @@ class Game
         ##### Enter the squares for the Submarine (2 spaces):
         ##### C1 C3
         ##### Those are invalid coordinates. Please try again:
-
+         
+        
+        # while crusier_p.sunk? != true && submarine_p.sunk? != true || crusier_c.sunk? != true && submarine_c.sunk? != true
         ### user is prompted to pick a location to fire at beginning the game (maybe use loop)
         ### each time the there is a fire action the boards are 
         ### rerendered with the new info being displayed 
         ### feedback is also given in the form of a printed statement 
+          # end 
 
-
-        ### 
+        ### END GAME 
 
 
 
@@ -45,3 +68,7 @@ class Game
 
   end
 end
+
+
+
+# print "These are the game boards.\nThe top one is mine \nand the bottom is yours.\n" + computer.render + "\n" +  "===========\n" + player.render(true) 
